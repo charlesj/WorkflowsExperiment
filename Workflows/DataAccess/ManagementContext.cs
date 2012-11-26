99651f41abb1fa48d81be5a3cf923b7f922e3f1d
Josh Charles <josh.charles@gmail.com>
2012-11-25 22:51:35 -0800

    Fleshing out locator idea.

--- Workflows/DataAccess/ManagementContext.cs
+++ Workflows/DataAccess/ManagementContext.cs
@@ -45,9 +45,28 @@ namespace WorkflowsExperiment.DataAccess
 			
 		}
 
-		public T Locate<T>(params object[] args)
+		public T Find<T>(params object[] args)
 		{
 			// check to see if locator for type is loaded.  If not, load it
+			throw new NotImplementedException("Locator not yet implemented");
+
+			// okay the idea here is simple
+			// there are locators that all inherit from Locatorbase
+			// I can look for a type (and store a reference to it in case I need it later)
+			// I can look for a Find() method on it that uses arguments with the type as the arguments passed into this 
+			// then I call that method and return it on this function
+			// so example usage: var org = db.Find<Organization>("Hogwarts");
+
+			// in this case, I would look for an Organization Locator.  If it's found, this I look for a method called "Find" 
+			// with a type signature of (string) and a return type of Organization (check return type just to be safe)
+			// that gets found, then I call that method and return the results.
+
+			// things *could* get a bit tricky with argument ordering.  perhaps there is a fit for dynamic in here.
+		}
+
+		public List<T> FindMultiple<T>(params object[] args)
+		{
+			// same idea as above, but for multiple results
 			throw new NotImplementedException("Locator not yet implemented");
 		}
 	}
