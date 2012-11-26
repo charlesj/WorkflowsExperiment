bae9d5ceda5001acceafa84c94b4eef856d0f4d8
Josh Charles <josh.charles@gmail.com>
2012-11-25 22:36:46 -0800

    Tested Pace order workflow with product with no application.

--- WorkflowsExperiements.Tests/WorkFlowIntegrationTests.cs
+++ WorkflowsExperiements.Tests/WorkFlowIntegrationTests.cs
@@ -3,7 +3,6 @@
 	using System;
 	using System.Linq;
 
-	using WorkflowsExperiment.DataAccess;
 	using WorkflowsExperiment.Tests.ReferenceInstances;
 	using WorkflowsExperiment.Workflows.CompleteWorkflows;
 
@@ -12,11 +11,28 @@
 	public class WorkFlowIntegrationTests
 	{
 		[Fact]
-		public void Play()
+		public void PlaceOrderWorkflowWithProductWithApplication()
 		{
 			var db = BeginningDb.Build();
 			var beneficiary = db.Organizations.Single(org => org.Name == "Hogwarts");
 			var product = db.Products.Single(prod => prod.Name == "SWIS Subscription");
+			var payer = db.Organizations.Single(org => org.Name == "Hogwarts");
+
+			var workflow = new PlaceOrderWorkflow(db, beneficiary, product, payer);
+
+			workflow.GetPreWorkDescription().ForEach(Console.WriteLine);
+
+			var result = workflow.Execute();
+
+			result.DescribeResults().ForEach(Console.WriteLine);
+		}
+
+		[Fact]
+		public void PlaceOrderWorkflowWithProductWithoutApplication()
+		{
+			var db = BeginningDb.Build();
+			var beneficiary = db.Organizations.Single(org => org.Name == "Hogwarts");
+			var product = db.Products.Single(prod => prod.Name == "SWIS Training");
 			var payer = db.Organizations.Single(org => org.Name == "Hogwarts");
 
 			var workflow = new PlaceOrderWorkflow(db, beneficiary, product, payer);
